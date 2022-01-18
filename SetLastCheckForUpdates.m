e4dfae9e94b84494db11ec8396394388fa9bf9b1
jayd1860 <44243610+jayd1860@users.noreply.github.com>
2022-01-18 16:15:34 -0500

    Development (#7)
    * -- Add hash function for strings for facilitating new capability to read data sets in a BIDS folder structure.
    -- Remove obsolete functions that were used with the older clunkier setpaths
    
    * Update number of files in library
    
    * -- Fix bug in check for Homer3 updates when LastCheckForUpdates.dat file expires.

--- SetLastCheckForUpdates.m
+++ SetLastCheckForUpdates.m
@@ -1,13 +1,8 @@
-function SetLastCheckForUpdates(dt)
-if ~exist('dt','var') && ispathvalid([getAppDir, 'LastCheckForUpdates.dat'])
-    return;
-end
-if ~exist('dt','var')
-    try
-        dt = datetime;
-    catch
-        dt = -1;
-    end
+function SetLastCheckForUpdates()
+try
+    dt = datetime;
+catch
+    dt = -1;
 end
 fd = fopen([getAppDir, 'LastCheckForUpdates.dat'],'wt');
 fprintf(fd, '%s\n', dt);
